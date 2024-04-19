04bfb386147d6e7ae05a8616452c785d147ffc99
Kirill Bunin <bunin.ka@phystech.edu>
2024-04-19 14:56:08 +0300

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vitaly/local_planner_ws/src
+# Build directory: /home/vitaly/local_planner_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("MPC_Local_Planner")
---
+++ build/MPC_Local_Planner/CMakeFiles/mpc_planner.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/home/vitaly/local_planner_ws/devel/lib/mpc_planner/mpc_planner"
+  "/home/vitaly/local_planner_ws/devel/lib/mpc_planner/mpc_planner.pdb"
+  "CMakeFiles/mpc_planner.dir/src/MPC_Planner.cpp.o"
+  "CMakeFiles/mpc_planner.dir/src/MPC_Planner.cpp.o.d"
+  "CMakeFiles/mpc_planner.dir/src/MPC_ROS_Utils.cpp.o"
+  "CMakeFiles/mpc_planner.dir/src/MPC_ROS_Utils.cpp.o.d"
+  "CMakeFiles/mpc_planner.dir/src/main.cpp.o"
+  "CMakeFiles/mpc_planner.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mpc_planner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
